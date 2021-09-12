f0caeca244965fac600720ce35738595e0582e00
Callum Lawson-Smith <clawsonsmith@au.palo-it.com>
2021-09-12 15:10:10 +1000

    use environment variables

--- src/api/getEpochDetails.ts
+++ src/api/getEpochDetails.ts
@@ -3,7 +3,7 @@ import axios, { AxiosResponse } from 'axios';
 const getEpochDetails = async (epoch: string): Promise<AxiosResponse> => {
 	return axios.get(`https://cardano-mainnet.blockfrost.io/api/v0/epochs/${epoch}`, {
 		headers: {
-			project_id: '5k4f6Bkg2T1VVgnRMYMwD69KCemDBpwU' 
+			project_id: process.env.REACT_APP_BLOCKFROST_API_KEY
 		}
 	});
 };
--- src/api/getStakingHistory.ts
+++ src/api/getStakingHistory.ts
@@ -3,7 +3,7 @@ import axios, { AxiosResponse } from 'axios';
 const getStakingHistory = async (rewardsAddress: string): Promise<AxiosResponse> => {
 	return axios.get(`https://cardano-mainnet.blockfrost.io/api/v0/accounts/${rewardsAddress}/rewards`, {
 		headers: {
-			project_id: '5k4f6Bkg2T1VVgnRMYMwD69KCemDBpwU'
+			project_id: process.env.REACT_APP_BLOCKFROST_API_KEY
 		}
 	});
 };
